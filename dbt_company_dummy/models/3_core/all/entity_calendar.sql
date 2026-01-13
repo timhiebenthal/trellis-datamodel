478c6ff99109c95ded31a92f83a186700dcf0e97
Tim Hiebenthal <tim.hiebenthal@a11.global>
2026-01-13 09:08:02 +0100

    fix: update date formatting in calendar SQL queries
    - Changed the date casting in `prep_calendar.sql` to use `current_timestamp` for consistency.
    - Updated `entity_calendar.sql` to use `current_date` and corrected the strftime function calls for better accuracy in date representation.

--- dbt_company_dummy/models/3_core/all/entity_calendar.sql
+++ dbt_company_dummy/models/3_core/all/entity_calendar.sql
@@ -1,6 +1,6 @@
 select 
-    cast(date('now') as text) as calendar_date,
-    cast(strftime('%Y-W%V', 'now') as text) as calendar_week,
-    cast(strftime('%Y-%m', 'now') as text) as year_month
+    cast(current_date as text) as calendar_date,
+    cast(strftime(current_timestamp, '%Y-W%V') as text) as calendar_week,
+    cast(strftime(current_timestamp, '%Y-%m') as text) as year_month
 limit 1
 
